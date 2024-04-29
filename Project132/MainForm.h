fc140d76211c1bfddbda554fb27339de6e313b6f
Emaan Fatima <146338575+emaanfatim@users.noreply.github.com>
2024-04-29 16:15:51 +0500

    Update MainForm.h

--- Project132/MainForm.h
+++ Project132/MainForm.h
@@ -3,6 +3,8 @@
 #include"Teacherwise.h"
 #include"Classroomwise.h"
 #include"Sectionwise.h"
+#include"RoomArranger.h"
+
 namespace Project132 {
 
 	using namespace System;
@@ -41,6 +43,10 @@ namespace Project132 {
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::ColorDialog^ colorDialog1;
+	private: System::Windows::Forms::Button^ button6;
 	protected:
 
 	private:
@@ -60,11 +66,15 @@ namespace Project132 {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->colorDialog1 = (gcnew System::Windows::Forms::ColorDialog());
+			this->button6 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(62, 38);
+			this->button1->Location = System::Drawing::Point(206, 70);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(129, 48);
 			this->button1->TabIndex = 0;
@@ -74,7 +84,7 @@ namespace Project132 {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(62, 106);
+			this->button2->Location = System::Drawing::Point(206, 149);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(129, 48);
 			this->button2->TabIndex = 1;
@@ -84,7 +94,7 @@ namespace Project132 {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(62, 171);
+			this->button3->Location = System::Drawing::Point(206, 213);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(129, 48);
 			this->button3->TabIndex = 2;
@@ -94,7 +104,7 @@ namespace Project132 {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(62, 236);
+			this->button4->Location = System::Drawing::Point(206, 290);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(129, 48);
 			this->button4->TabIndex = 3;
@@ -102,11 +112,46 @@ namespace Project132 {
 			this->button4->UseVisualStyleBackColor = true;
 			this->button4->Click += gcnew System::EventHandler(this, &MainForm::button4_Click);
 			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(206, 368);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(129, 48);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"RoomArranger";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MainForm::button5_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(116, 21);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(373, 29);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Bahria University H-11 Campus";
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(216, 445);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(118, 43);
+			this->button6->TabIndex = 6;
+			this->button6->Text = L"Exit";
+			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &MainForm::button6_Click);
+			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 338);
+			this->BackColor = System::Drawing::SystemColors::ControlLight;
+			this->ClientSize = System::Drawing::Size(517, 551);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
@@ -114,6 +159,7 @@ namespace Project132 {
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -136,5 +182,22 @@ private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e
 	Sectionwise obj;
 	obj.ShowDialog();
 }
+private: System::Void button6_Click(System::Object^ sender, System::EventArgs^ e) {
+	// Show a message box with a confirmation dialog
+	System::Windows::Forms::DialogResult result = MessageBox::Show("Are you sure you want to Quit? ", "Confirmation", MessageBoxButtons::OKCancel, MessageBoxIcon::Question);
+
+	// Check if the user clicked OK
+	if (result == System::Windows::Forms::DialogResult::OK) {
+		// If yes, exit the application
+		Application::Exit();
+	}
+}
+
+
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+	RoomArranger obj;
+	obj.ShowDialog();
+}
 };
 }
+
